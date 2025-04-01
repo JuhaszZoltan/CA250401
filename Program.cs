6aa278e0c8c7e55efcb9b1562418ac7b30fc0454
Juhász Zoltán <JuhaszZoltanMate@yahoo.com>
2025-04-01 17:25:16 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿const string PATH = "..\\..\\..\\src\\valami.txt";
+
+Console.WriteLine("hello world!");
+
+Console.WriteLine("Wanna' read valami.txt? y/n");
+
+if (Console.ReadKey(true).KeyChar == 'y')
+{
+    StreamReader sr = new("valami.txt");
+    Console.WriteLine(sr.ReadLine());
+}
+
+Console.WriteLine("press ESC to exit");
+
+while (Console.ReadKey(true).Key != ConsoleKey.Escape) ;
+
+
